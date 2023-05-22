2cf699679b008abf4a974da85ff80dcceb0ec004
yu-katayama <katayu810@gmail.com>
2023-05-22 22:27:22 +0900

    クラウド化により、backend不要

--- backend-node/models/person.js
+++
@@ -1,8 +0,0 @@
-const {model, Schema} = require("mongoose");
-
-const messageSchema = new Schema({
-	name: String,
-	age: Number,
-})
-
-module.exports = model("person", messageSchema);
--- backend-node/models/tweet.js
+++
@@ -1,9 +0,0 @@
-const {model, Schema} = require("mongoose");
-
-const messageSchema = new Schema({
-	description: String,
-	like: Number,
-	person: Schema.Types.ObjectId,
-})
-
-module.exports = model("tweet", messageSchema);
